a68338eb6bb4ee005ae21b37d75b4adacde4f941
Shakeel Haider <shkhaider2015@gmail.com>
2023-08-13 19:02:32 +0500

    find duplicate

--- problemSolving/duplicate/index.ts
+++ problemSolving/duplicate/index.ts
@@ -22,11 +22,29 @@ const data1:IData[] = [
         }
     },
     {
+        id: 1,
+        name: 'Shakeel Haider',
+        age: 30,
+        address: {
+            city: 'Karachi',
+            country: 'Pakistan'
+        }
+    },
+    {
         id: 2,
         name: 'Leory Sane',
         age: 26,
         address: {
             city: 'Munich',
+            country: 'Germany'
+        }
+    },
+    {
+        id: 3,
+        name: 'Emre Can',
+        age: 28,
+        address: {
+            city: 'Dortmund',
             country: 'Germany'
         }
     },
@@ -50,4 +68,23 @@ const data1:IData[] = [
     }
 ]
 
+let index:number = 0;
+let newArray:IData[] = [];
+const dataLength:number = data1.length;
+
+for(let i=0; i<dataLength -1; i++)
+{
+    for(let j=i+1; j<dataLength; j++)
+    {
+        if(data1[i].id === data1[j].id)
+        {
+            newArray[index] = data1[i];
+            index++;
+        }
+    }
+}
+
+console.log("============= Original Data ===================");
 console.table(data1)
+console.log("============= Duplicates ===================");
+console.table(newArray)
